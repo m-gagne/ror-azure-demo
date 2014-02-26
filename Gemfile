f5fc1d3f752a83427e509c57b9be359c75e2cc1e
Marc Gagne <marc.gagne@hotmail.com>
2014-02-26 14:52:07 -0500

    Getting production deployment setup

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
+# Use mysql in production
+gem 'mysql'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,24 @@
 # Capistrano deployment setup based on tutorial by Ben Dixon / @talkingquickly
 # http://www.talkingquickly.co.uk/2014/01/deploying-rails-apps-to-a-vps-with-capistrano-v3/
 
-
-# name of your application
 set :application, 'ror-azure-demo'
-
-# name of your user account
-# default for Windows Azure Virtual Machines is 'azureuser'
 set :deploy_user, 'azureuser'
 
-# setup github deployment information
+# setup repo details
 set :scm, :git
 set :repo_url, 'https://github.com/m-gagne/ror-azure-demo.git'
 
-# setup rvm
+# setup rvm.
 set :rbenv_type, :user
 set :rbenv_ruby, '2.0.0-p451'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
-# how many old releases do we want to keep
+# how many old releases do we want to keep, not much
 set :keep_releases, 5
 
 # files we want symlinking to specific entries in shared
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -31,6 +26,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
 #set :tests, ["spec"]
+set :tests, []
 
 # which config files should be copied by deploy:setup_config
 # see documentation in lib/capistrano/tasks/setup_config.cap
@@ -51,28 +47,30 @@ set(:executable_config_files, %w(
   unicorn_init.sh
 ))
 
+
 # files which need to be symlinked to other parts of the
 # filesystem. For example nginx virtualhosts, log rotation
-# init scripts etc.
+# init scripts etc. The full_app_name variable isn't
+# available at this point so we use a custom template {{}}
+# tag and then add it at run time.
 set(:symlinks, [
   {
     source: "nginx.conf",
-    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
+    link: "/etc/nginx/sites-enabled/{{full_app_name}}"
   },
   {
     source: "unicorn_init.sh",
-    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
+    link: "/etc/init.d/unicorn_{{full_app_name}}"
   },
   {
     source: "log_rotation",
-   link: "/etc/logrotate.d/#{fetch(:full_app_name)}"
-  },
-  {
-    source: "monit",
-    link: "/etc/monit/conf.d/#{fetch(:full_app_name)}.conf"
-  }
+   link: "/etc/logrotate.d/{{full_app_name}}"
+  }#,
+  #{
+  # source: "monit",
+  #  link: "/etc/monit/conf.d/{{full_app_name}}.conf"
+  #}
 ])
-
 
 # this:
 # http://www.capistranorb.com/documentation/getting-started/flow/
@@ -83,7 +81,7 @@ namespace :deploy do
   # make sure we're deploying what we think we're deploying
   before :deploy, "deploy:check_revision"
   # only allow a deploy with passing tests to deployed
-  #before :deploy, "deploy:run_tests"
+  before :deploy, "deploy:run_tests"
   # compile assets locally then rsync
   after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
